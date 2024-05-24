61ee199263a976f22e2012832f57602ff32e5530
CuongBap <89674054+CuongBap@users.noreply.github.com>
2024-05-24 23:46:00 +0700

    CRUD MenuFoodItem

---
+++ FlashFoodDelivery/Controllers/MenuItemFoodController.cs
@@ -0,0 +1,40 @@
+﻿using Business_Layer.Repositories;
+using Data_Layer.ResourceModel.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemFoodController : ControllerBase
+    {
+        public IMenuFoodItemRepository _menuFoodItemRepository;
+
+        public MenuItemFoodController(IMenuFoodItemRepository menuFoodItemRepository)
+        {
+            _menuFoodItemRepository = menuFoodItemRepository;
+        }
+
+        [HttpGet]
+        public async Task<APIResponseModel> GetAllMenuFoodItem()
+        {
+            try
+            {
+                var result = await _menuFoodItemRepository.GetMenuFoodItem();
+                return new APIResponseModel()
+                {
+                    code = 200,
+                    message = "Get successful",
+                    IsSuccess = true,
+                    Data = result,
+                };
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
