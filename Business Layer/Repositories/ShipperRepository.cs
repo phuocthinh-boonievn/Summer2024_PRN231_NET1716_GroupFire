e2e39c0d783045bb52c1c08da1994ccc52b5a2e9
Phuoc Thinh <phuocthinhlkr@gmail.com>
2024-05-30 08:11:11 +0700

    base shipper repo

---
+++ Business Layer/Repositories/ShipperRepository.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Business_Layer.DataAccess;
+using Data_Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.Repositories
+{
+    public class ShipperRepository : GenericRepository<Shipper>, IShipperRepository
+    {
+        private readonly IMapper _mapper;
+        private readonly FastFoodDeliveryDBContext _dbContext;
+        public ShipperRepository(FastFoodDeliveryDBContext context, FastFoodDeliveryDBContext dbContext) : base(context)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<IEnumerable<Order>> GetAllByStatusAsync(string status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
