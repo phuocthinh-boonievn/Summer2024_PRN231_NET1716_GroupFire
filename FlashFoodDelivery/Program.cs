edfedf56a35249367bb46572b380293418d71be5
Phuoc Thinh <phuocthinhlkr@gmail.com>
2024-06-22 17:49:04 +0700

    Add initial vnpay functions

--- FlashFoodDelivery/Program.cs
+++ FlashFoodDelivery/Program.cs
@@ -1,6 +1,12 @@
 using API;
+using Business_Layer.Services.VNPay;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// VNPay setting 
+builder.Services.AddSingleton<VNPayHelper>();
+builder.Services.Configure<VNPaySettings>(configuration.GetSection("VNPaySettings"));
 
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
